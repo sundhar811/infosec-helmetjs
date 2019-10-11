19630813a9802c1b6d260579d28ba29902304132
Glitch (sundhar811-infosec-helmetjs) <none>
2019-10-11 02:42:00 +0000

    Disable DNS Prefetching with helmet.dnsPrefetchControl()

--- myApp.js
+++ myApp.js
@@ -132,7 +132,7 @@ app.use(helmet.hsts({ maxAge: ninetyDaysInMilliseconds, force: true }));
 // DNS prefetching, at the cost of a performance penalty.
 
 // Use `helmet.dnsPrefetchControl()`
-
+app.use(helmet.dnsPrefetchControl({ allow: false }));
 
 
 /** 9) Disable Client-Side Caching - `helmet.noCache()` */
